61b5b19f54fed2bc70865b10b5311e72a615e50a
Chang Liu <liuc223@gmail.com>
2023-10-05 19:50:44 +0800

    Ignore vscode

---
+++ src/components/SchemaView/SchemaPropertyEditCell.vue
@@ -0,0 +1,43 @@
+<template>
+  <td :colspan="colspan">
+
+
+  </td>
+</template>
+
+<script lang="js">
+import { DATA_TYPES, INTERNAL_DATA_TYPES } from '../../utils/Constants';
+
+export default {
+  name: "SchemaPropertyEditCell",
+  data: () => {
+    const dataTypes = Object.keys(DATA_TYPES).filter(k => !INTERNAL_DATA_TYPES.includes(k));
+    return {
+      dataTypes,
+    }
+  },
+  props: {
+    property: {
+      type: Object,
+      required: true,
+    },
+    colspan: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    isNew: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  computed: {
+
+  }
+}
+
+
+</script>
+
+<style lang="scss" scoped></style>
