ae596180885b4dc71937e078e5f99cbf753b4af5
囧囧 <liuc223@gmail.com>
2023-10-23 17:51:42 +0800

    Address issues (#31)
    * Update kuzu to 0.0.11
    
    * Address #20

--- src/server/Schema.js
+++ src/server/Schema.js
@@ -2,30 +2,12 @@ const database = require("./utils/database");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const conn = database.getConnection();
+router.get("/", async (_, res) => {
   try {
-    const nodeTableNames = await conn.getNodeTableNames();
-    const relTableNames = await conn.getRelTableNames();
-    const nodeTableSchemas = await Promise.all(
-      nodeTableNames.map((table) => conn.getNodePropertyNames(table))
-    );
-    const nodeTables = nodeTableNames.map((table, i) => ({
-      name: table,
-      properties: nodeTableSchemas[i],
-    }));
-    const relTableSchemas = await Promise.all(
-      relTableNames.map((table) => conn.getRelPropertyNames(table))
-    );
-    const relTables = relTableNames.map((table, i) => ({
-      name: table,
-      properties: relTableSchemas[i],
-    }));
-    res.send({ nodeTables, relTables });
+    const schema = await database.getSchema();
+    res.send(schema);
   } catch (err) {
     return res.status(400).send({ error: err.message });
-  } finally {
-    database.releaseConnection(conn);
   }
 });
 
