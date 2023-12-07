da836e6b37cd6bd6231ee8b6a51fc9890b04a15e
Ashley Huo <x2huo@uwaterloo.ca>
2023-12-07 14:17:36 -0500

    clear .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
     extends: [
-        // add more generic rulesets here, such as:
-        // 'eslint:recommended',
-        // "plugin:vue/base",
         'plugin:vue/vue3-recommended',
         // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
     ],
     parserOptions: {
-        ecmaVersion: 2022, // Add this line
+        ecmaVersion: 2022,
     },
     rules: {
         // override/add rules settings here, such as:
