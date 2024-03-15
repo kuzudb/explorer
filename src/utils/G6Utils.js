682432239bc0f73b75315d2bfa73fd99456b6d37
囧囧 <liuc223@gmail.com>
2024-03-15 23:22:58 +0800

    Improve schema view layout (#101)
    * Improvements to schema view layout
    
    * Run linters
    
    ---------
    
    Co-authored-by: Lint Bot <mewim@users.noreply.github.com>

--- src/utils/G6Utils.js
+++ src/utils/G6Utils.js
@@ -77,6 +77,30 @@ class G6Utils {
     }
     return str;
   }
+
+  shadeColor(color, percent) {
+    let R = parseInt(color.substring(1, 3), 16);
+    let G = parseInt(color.substring(3, 5), 16);
+    let B = parseInt(color.substring(5, 7), 16);
+
+    R = parseInt((R * (100 + percent)) / 100);
+    G = parseInt((G * (100 + percent)) / 100);
+    B = parseInt((B * (100 + percent)) / 100);
+
+    R = R < 255 ? R : 255;
+    G = G < 255 ? G : 255;
+    B = B < 255 ? B : 255;
+
+    R = Math.round(R);
+    G = Math.round(G);
+    B = Math.round(B);
+
+    let RR = R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
+    let GG = G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
+    let BB = B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
+
+    return "#" + RR + GG + BB;
+  }
 }
 
 const g6Utils = new G6Utils();
