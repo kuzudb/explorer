8945ddb5053c4f0fec70bea61499f89b1da3f891
囧囧 <liuc223@gmail.com>
2024-03-25 19:57:36 +0800

    Add nightly build schedule and update dev version in prebuild script (#121)

---
+++ scripts/UpdateDevVersion.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const exec = require("child_process").exec;
+const process = require("process");
+
+const PACKAGE_JSON_PATH = path.resolve(__dirname, "../package.json");
+const CMD = "npm view kuzu@next version";
+
+(() => {
+  exec(CMD, (error, stdout, _) => {
+    if (error) {
+      console.error(`Failed to get latest dev version: ${error}`);
+      process.exit(1);
+    }
+    const latestVersion = stdout.trim();
+    console.log(`Latest dev version is ${latestVersion}`);
+    fs.readFile(PACKAGE_JSON_PATH, "utf8", (err, data) => {
+      console.log("Opened package.json at", PACKAGE_JSON_PATH);
+      if (err) {
+        console.error("Error reading package.json", err);
+        process.exit(1);
+      }
+      const packageJson = JSON.parse(data);
+      packageJson.dependencies.kuzu = `^${latestVersion}`;
+      fs.writeFile(
+        PACKAGE_JSON_PATH,
+        JSON.stringify(packageJson, null, 2),
+        (err) => {
+          if (err) {
+            console.error("Error writing package.json", err);
+            process.exit(1);
+          }
+          console.log(`Updated kuzu to ${latestVersion}`);
+          process.exit(0);
+        }
+      );
+    });
+  });
+})();
