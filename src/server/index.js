93ab434d18df4257bc9b83cc61e1c120f057332d
Chang Liu <liuc223@gmail.com>
2024-03-29 09:16:39 +0800

    Fix version logging for dev builds

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const process = require("process");
 const database = require("./utils/Database");
 const logger = require("./utils/Logger");
+const fs = require("fs");
 
 process.on("SIGINT", () => {
   logger.info("SIGINT received, exiting");
@@ -23,15 +24,38 @@ const distPath = path.join(__dirname, "..", "..", "dist");
 app.use("/", express.static(distPath, { maxAge: "30d" }));
 
 const conn = database.getConnection();
-conn
-  .query("CALL db_version() RETURN *;")
-  .then((res) => {
-    return res.getAll();
-  })
-  .then((res) => {
-    const row = res[0];
-    const version = Object.values(row)[0];
+
+const getDbVersionFromQuery = (conn) => {
+  return conn
+    .query("CALL db_version() RETURN *;")
+    .then((res) => {
+      return res.getAll();
+    })
+    .then((res) => {
+      const row = res[0];
+      const version = Object.values(row)[0];
+      return version;
+    });
+};
+
+const getDbVersionFromPackage = () => {
+  const packagePath = path.join(__dirname, "..", "..", "package.json");
+  return fs.promises.readFile(packagePath, "utf8").then((data) => {
+    const packageJson = JSON.parse(data);
+    return packageJson.dependencies.kuzu;
+  });
+};
+
+Promise.all([getDbVersionFromQuery(conn), getDbVersionFromPackage()])
+  .then(([queryVersion, packageVersion]) => {
+    // We do not actively maintain the version for dev builds, so if package
+    // version includes "dev" we take the package version as the source of truth
+    const version = packageVersion.includes("dev")
+      ? packageVersion
+      : queryVersion;
     logger.info("Version of Kùzu: " + version);
+    const storageVersion = db.kuzu.STORAGE_VERSION;
+    logger.info("Storage version of Kùzu: " + storageVersion);
     app.listen(PORT, () => {
       logger.info("Deployed server started on port: " + PORT);
     });
